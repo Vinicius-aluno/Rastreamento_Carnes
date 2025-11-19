0c7f7968e31d8c26d4c49b8b6b4eee448e07e204
Otávio Miguel Abreu Martins <otavio.martins25@etec.sp.gov.br>
2025-11-19 18:07:58 -0300

    Banco de dados upado para o Helton

--- bd_php_inicio.sql
+++ bd_php_inicio.sql
@@ -2,17 +2,28 @@ CREATE DATABASE rastreamento_carnes;
 USE rastreamento_carnes;
 
 /*=====================================
-	 TABELA DE USUÁRIOS
+	 TABELA DE USUÁRIOS E CARGOS
 =====================================*/
 
-CREATE TABLE usuarios (
-id INT AUTO_INCREMENT PRIMARY KEY,
-nome VARCHAR(100) NOT NULL,
-email VARCHAR(120) NOT NULL UNIQUE,
-senha VARCHAR(255) NOT NULL,
-cargo ENUM('suporte','funcionario','gerente','admin') NOT NULL
+CREATE TABLE cargos (
+id_cargo INT PRIMARY KEY,
+nome_cargo VARCHAR(50)
 );
 
+CREATE TABLE usuario (
+id_usuario INT PRIMARY KEY,
+nome_usuario VARCHAR(120),
+email_usuario VARCHAR(120),
+senha_usuario VARCHAR(255)
+);
+
+CREATE TABLE usuario_tem_cargos (
+id_usuario INT,
+id_cargo INT,
+PRIMARY KEY (id_usuario, id_cargo),
+FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario),
+FOREIGN KEY (id_cargo) REFERENCES cargos(id_cargo)
+);
 /*=====================================
 	 TABELA DE FORNECEDORES
 =====================================*/
@@ -21,6 +32,14 @@ CREATE TABLE fornecedores (
 id INT AUTO_INCREMENT PRIMARY KEY,
 nome VARCHAR(150) NOT NULL,
 cnpj VARCHAR(20) NOT NULL UNIQUE
+);
+
+CREATE TABLE fornecedores_tem_lotes (
+id_fornecedor INT,
+id_lote INT,
+PRIMARY KEY (id_fornecedor, id_lote),
+FOREIGN KEY (id_fornecedor) REFERENCES fornecedores(id_fornecedor),
+FOREIGN KEY (id_lote) REFERENCES lotes(id_lote)
 );
 
 /*=====================================
@@ -57,30 +76,69 @@ CONSTRAINT FOREIGN KEY (produto_id) REFERENCES produtos(id),
 CONSTRAINT FOREIGN KEY (fornecedor_id) REFERENCES fornecedores(id)
 );
 
+CREATE TABLE produtos_nos_lotes (
+id_produto INT,
+id_lote INT,
+quantidade INT,
+PRIMARY KEY (id_produto, id_lote),
+FOREIGN KEY (id_produto) REFERENCES produtos(id_produto),
+FOREIGN KEY (id_lote) REFERENCES lotes(id_lote)
+);
+
+CREATE TABLE status_lote (
+id_status INT PRIMARY KEY,
+nome_status VARCHAR(40)
+);
+
+CREATE TABLE lote_status_historico (
+id_lote_status INT PRIMARY KEY,
+id_lote INT NOT NULL,
+id_status INT NOT NULL,
+id_usuario INT NOT NULL,
+data_evento DATETIME,
+FOREIGN KEY (id_lote) REFERENCES lotes(id_lote),
+FOREIGN KEY (id_status) REFERENCES status_lote(id_status),
+FOREIGN KEY (id_usuario) REFERENCES usuarios(id)
+);
+
 
 /*=====================================
  TABELA DE TEMPERATURAS DO LOTE
 =====================================*/
 
-CREATE TABLE temperaturas (
-id INT AUTO_INCREMENT PRIMARY KEY,
-lote_id INT NOT NULL,
-temperatura DECIMAL(5,2) NOT NULL,
-data_registro DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-CONSTRAINT FOREIGN KEY (lote_id) REFERENCES lotes(id)
+CREATE TABLE tipo_sensores (
+id_tipo INT PRIMARY KEY,
+nome_tipo VARCHAR(20)
 );
+
+CREATE TABLE sensores (
+id_sensor INT PRIMARY KEY,
+id_tipo INT NOT NULL,
+id_veiculo INT NOT NULL,
+FOREIGN KEY (id_tipo) REFERENCES tipo_sensores(id_tipo),
+FOREIGN KEY (id_veiculo) REFERENCES veiculos(id_veiculo)
+);
+
+CREATE TABLE temperaturas (
+id_temperatura INT PRIMARY KEY,
+id_sensor INT NOT NULL,
+temp_reg DECIMAL(5,2),
+data_reg DATETIME,
+FOREIGN KEY (id_sensor) REFERENCES sensores(id_sensor)
+);
+
 
 /*=====================================
  TABELA DE EVENTOS DO LOTE
 =====================================*/
 
 CREATE TABLE eventos (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    lote_id INT NOT NULL,
-    tipo VARCHAR(50) NOT NULL,
-    descricao TEXT,
-    data_evento DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (lote_id) REFERENCES lotes(id)
+id INT AUTO_INCREMENT PRIMARY KEY,
+lote_id INT NOT NULL,
+tipo VARCHAR(50) NOT NULL,
+descricao TEXT,
+data_evento DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+FOREIGN KEY (lote_id) REFERENCES lotes(id)
 );
 
 create table tipo_eventos (
@@ -129,6 +187,13 @@ id_logradouro int not null,
 foreign key (id_logradouro) references logradouro (id_logradouro)
 );
 
+CREATE TABLE enderecos (
+id_endereco INT PRIMARY KEY,
+id_cep INT NOT NULL,
+complemento_end VARCHAR(150),
+FOREIGN KEY (id_cep) REFERENCES cep(id_cep)
+);
+
 /*=====================================
 TABELAS DOS VEÍCULOS DE TRANSPORTE
 ======================================*/
@@ -146,15 +211,35 @@ placa_veiculo varchar (10),
 modelo_veiculo varchar (80)
 );
 
-/*======================================
-TABELA USUÁRIO
-========================================*/
-create table usuario ( 
-id_usuario int not null,
-nome_usuario varchar (120),
-email_usuario varchar (120),
-senha_usuario varchar (255)
+CREATE TABLE lotes_veiculos (
+id_lote INT,
+id_veiculo INT,
+data DATE,
+PRIMARY KEY (id_lote, id_veiculo),
+FOREIGN KEY (id_lote) REFERENCES lotes(id_lote),
+FOREIGN KEY (id_veiculo) REFERENCES veiculos(id_veiculo)
 );
+
+
+/*=====================================
+TABELA COMPRADORES
+=====================================*/
+CREATE TABLE compradores (
+id_comprador INT PRIMARY KEY,
+nome_comprador VARCHAR(120),
+cpf_comprador VARCHAR(20),
+id_endereco INT NOT NULL,
+FOREIGN KEY (id_endereco) REFERENCES enderecos(id_endereco)
+);
+
+CREATE TABLE comprador_tem_lotes (
+id_comprador INT,
+id_lote INT,
+PRIMARY KEY (id_comprador, id_lote),
+FOREIGN KEY (id_comprador) REFERENCES compradores(id_comprador),
+FOREIGN KEY (id_lote) REFERENCES lotes(id_lote)
+);
+
 
 /*=====================================
 	 USUÁRIOS
@@ -206,4 +291,3 @@ VALUES
 (1, 'Carregado', 'Lote carregado no caminhão'),
 (1, 'Inspecionado', 'Inspeção realizada. Temperatura ok.'),
 (2, 'Carregado', 'Transportadora iniciou deslocamento');
-
